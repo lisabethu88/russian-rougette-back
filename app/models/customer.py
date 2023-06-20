c39a8ba31f89ddff19c9bb66ffbc16663f6cfe9c
Lisa Utsett <lisabethutsett@gmail.com>
2023-06-20 09:11:42 -0400

    Added customer routes for POST, PUT, GET, and DELETE. Changed customer id field to autoincrement.

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,7 +1,7 @@
 from app import db
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     name = db.Column(db.String)
     email = db.Column(db.String) 
--- app/routes/customer_routes.py
+++ app/routes/customer_routes.py
@@ -1,25 +1,98 @@
-from flask import Blueprint, request, jsonify, make_response, abort
+from flask import Blueprint, request, jsonify, make_response, abort, session
+from werkzeug.security import check_password_hash, generate_password_hash
 from app import db
 from app.models.customer import Customer
+from app.routes.helpers import validate_model
 
-customers_bp = Blueprint("customers_bp", __name__)
+customers_bp = Blueprint("customers_bp", __name__, url_prefix="/customers")
 
-# POST /add customer
+# POST /add a customer
 @customers_bp.route("/register", methods=["POST"])
 def create_customer():
     customer_data = request.get_json() 
     print(customer_data)
-    # check if customer exists
-    customer_exist = Customer.query.filter_by(email=customer_data["email"]).first() is not None
-    if customer_exist:
-        return make_response(jsonify({"error": "Customer already exists"}), 409)
 
+    # check if customer exists
+    email_exists = Customer.query.filter_by(email=customer_data["email"]).first() is not None
+    username_exists = Customer.query.filter_by(username=customer_data["username"]).first() is not None
+
+    if email_exists:
+        return make_response(jsonify({"error": "Email already exists"}), 409)
+    
+    if username_exists:
+        return make_response(jsonify({"error": "Username already exists"}), 409)
+
+        
     new_customer = Customer.from_dict(customer_data);
     
     db.session.add(new_customer)
     db.session.commit()
     
     # remember the customer 
-    # session["customer_id"] = new_customer.id
+    session["customer_id"] = new_customer.id
     
     return make_response(new_customer.to_dict(), 201)
+
+
+# GET /get a customer
+@customers_bp.route("/<int:customer_id>" , methods=["GET"])
+def get_current_user(customer_id):
+    # customer_id = session.get("customer_id")
+
+    if not customer_id:
+        return make_response(jsonify({"error": "Unauthorized"}), 401)
+    
+    customer = Customer.query.filter_by(id=customer_id).first()
+    return jsonify(customer.to_dict()) 
+
+# GET /get all customers
+@customers_bp.route('/', methods=['GET'])
+def get_customers():
+    customers = Customer.query.all()
+
+    customer_list = []
+    for customer in customers:
+        customer_list.append(customer.to_dict())
+
+    return jsonify(customer_list)
+
+
+# UPDATE /update a customer
+@customers_bp.route('/<int:customer_id>', methods=['PUT'])
+def update_customer(customer_id):
+    
+    customer = validate_model(Customer, customer_id)
+
+    data = request.get_json()
+
+    name = data.get('name')
+    email = data.get('email')
+    username = data.get('username')
+    password = data.get('password')
+    eyeshadows = data.get('eyeshadows')
+
+    if name:
+        customer.name = name
+    if email:
+        customer.email = email
+    if username:
+        customer.username = username
+    if password:
+        customer.password = password
+    if eyeshadows:
+        customer.eyeshadows = eyeshadows
+
+    db.session.commit()
+
+    return jsonify({'message': 'Customer updated successfully.'})
+
+# DELETE /delete a customer
+@customers_bp.route('/<int:customer_id>', methods=['DELETE'])
+def delete_customer(customer_id):
+
+    customer = validate_model(Customer, customer_id)
+
+    db.session.delete(customer)
+    db.session.commit()
+
+    return jsonify({'message': 'Customer deleted successfully.'})
